14484fdbe4cc89e4dc04e6f584243255a52affcf
cpe02.khanhkn <cpe02.khanhkn@techcombank.com.vn>
2024-04-22 17:39:12 +0700

    upd: st

--- app/src/main/java/com/koai/kingofenglish/ui/leaderBoad/LeaderBoardScreen.kt
+++ app/src/main/java/com/koai/kingofenglish/ui/leaderBoad/LeaderBoardScreen.kt
@@ -2,7 +2,9 @@ package com.koai.kingofenglish.ui.leaderBoad
 
 import android.os.Bundle
 import com.koai.base.main.extension.navigatorViewModel
+import com.koai.base.main.extension.screenViewModel
 import com.koai.base.main.screens.BaseScreen
+import com.koai.base.network.ResponseStatus
 import com.koai.kingofenglish.MainNavigator
 import com.koai.kingofenglish.R
 import com.koai.kingofenglish.databinding.ScreenLeaderBoardBinding
@@ -12,9 +14,25 @@ class LeaderBoardScreen :
     BaseScreen<ScreenLeaderBoardBinding, LeaderBoardRouter, MainNavigator>(R.layout.screen_leader_board) {
     override val navigator: MainNavigator by navigatorViewModel()
     private lateinit var adapter: LeaderBoardAdapter
+    private val viewmodel: LeaderBoardViewModel by screenViewModel()
 
     override fun initView(savedInstanceState: Bundle?, binding: ScreenLeaderBoardBinding) {
         adapter = LeaderBoardAdapter()
 
+        observer()
+        getData()
     }
+
+    private fun getData(){
+        viewmodel.getTopLeaders()
+    }
+
+    private fun observer(){
+        viewmodel.leaders.observe(this){
+            if(it is ResponseStatus.Success){
+                adapter.submitList(it.data.data)
+            }
+        }
+    }
+
 }
